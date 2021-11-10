6ef66eaca3be882a7c611e97a6d3021662837a0d
To Nhat Duy <duytngcc19188@fpt.edu.vn>
2021-11-10 20:00:53 +0700

    d

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
 <?php
-    $conn = pg_connect("postgres://vtqflwakisrnwx:19b2ede6da29c8e4ba30676e9f500d64b30b9ffea919f3c8cc382cecda59107e@ec2-18-208-24-104.compute-1.amazonaws.com:5432/d873l014qbmotv
-    ");
+    $conn = pg_connect("postgres://vtqflwakisrnwx:19b2ede6da29c8e4ba30676e9f500d64b30b9ffea919f3c8cc382cecda59107e@ec2-18-208-24-104.compute-1.amazonaws.com:5432/d873l014qbmotv");
 
     if(!$conn){
         die("Can not connect database");
